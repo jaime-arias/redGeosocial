e57b4717b27c176776bba0f5f7493c5a874575f7
Jaime Arias <jamestheavox@gmail.com>
2019-05-21 11:52:49 -0500

    Añadida interfaz de registro, inicio de sesión y pantalla de bienvenida

---
+++ pages/text.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<div data-role="main" class="ui-content">
+      <legend>Indica tu género</legend>
+      <label for="genre"><b>Indica tu género</b></label>
+
+        <label for="male">Masculino</label>
+        <input type="radio" name="gender" id="masculino" value="masculino">
+        <label for="female">Femenino</label>
+        <input type="radio" name="gender" id="femenino" value="femenino">
+        <label for="female">Otro</label>
+        <input type="radio" name="gender" id="otro" value="otro">
+  </div>
+</div>
+
+</html>
